d5818dab147f00f05bdc989c7521f0e48cf0defc
Valentino <valentino@dagherfam.com>
2021-02-03 21:34:44 -0600

    GOT A WORKING PROTOTYPE! Will need to add button to reset

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
-const gridElement = document.querySelector('.grid');
+const container = document.querySelector('.container');
 const gridSize = 256
+const color = 'red'
 
-
-//Create multiple divs
+//Div Generator
 function divGenerate(gridSize){
 for(let i = 0; i < gridSize; i++){
     let gridItems = document.createElement('div');
     gridItems.classList.add('gridItem')
-    gridElement.appendChild(gridItems);
+    container.appendChild(gridItems);
     console.log('genreated')
-}
+}}
 
-}
-
+//Generate Divs
 divGenerate(gridSize);
+
+
+const gridItems = document.querySelectorAll('.gridItem');
+
+function changeDivColor(color){
+    console.log(`Color Changed to ${color}`)
+     this.style.backgroundColor = "red";
+}
+
+gridItems.forEach(function(gridItem){
+    console.log('Binding gridItem');
+    gridItem.addEventListener('mouseover', changeDivColor)
+})
+
+
+// gridItems.forEach(function (gridItem){
+//     gridItem.addEventListener('click', changeDivColor);
+
+// })
